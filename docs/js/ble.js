97014f15dbc43925b00f533037076eafbd7ff55a
NoF0rte <NoF0rte@users.noreply.github.com>
2022-10-23 16:04:22 +0000

    deploy: 4a4bb74b1717ec13287e96174b2bd1b507205bc8

--- docs/js/ble.js
+++ docs/js/ble.js
@@ -1,166 +1,205 @@
-const BASE_API_URL = "http://localhost:8307/api"
-
-let isLoading = false
-const onLabel = "Turn On"
-const offLabel = "Turn Off"
-
-async function getPowerState(num) {
-	let state = await $.getJSON(`${BASE_API_URL}/powerstrip/${num}`)
-	return state.power
-}
-
-async function togglePowerState(num) {
-	if (isLoading) {
-		return
-	}
-
-	isLoading = true
-	showSpinner(`[data-outlet=${num}]`)
-
-	try {
-		let currentState = await getPowerState(num)
-		let resp = await $.ajax({
-			url: `${BASE_API_URL}/powerstrip/${num}`,
-			type: 'PUT',
-			data: JSON.stringify({
-				power: !currentState
-			})
-		});
-
-		if (!currentState) {
-			setButtonOff(num)
-		} else {
-			setButtonOn(num)
+const api = {
+	baseURL: "http://localhost:8307/api",
+	ping: async () => {
+		return await $.getJSON(`${api.baseURL}/ping`)
+	},
+	powerstrip: {
+		get: async (num) => {
+			let state = await $.getJSON(`${api.baseURL}/powerstrip/${num}`)
+			return state.power
+		},
+		set: async (num, power) => {
+			return await $.ajax({
+				url: `${api.baseURL}/powerstrip/${num}`,
+				type: 'PUT',
+				data: JSON.stringify({
+					power: power
+				})
+			});
 		}
-	
-		console.log(`Toggle power state of outlet ${num}:`)
-		console.log(resp)
-	} catch (error) {
-		console.log(error)
-	}
-
-	isLoading = false
-}
-
-function setButtonOn(num) {
-	let outlet = setButtonText(`[data-outlet=${num}]`, onLabel)
-	if (!outlet) {
-		return
-	}
-
-	outlet.removeClass("tip warning").addClass("tip")
-}
-
-function setButtonOff(num) {
-	let outlet = setButtonText(`[data-outlet=${num}]`, offLabel)
-	if (!outlet) {
-		return
-	}
-
-	outlet.removeClass("tip warning").addClass("warning")
-}
-
-function showSpinner(selector) {
-	let outlet = $(selector)
-	if (!outlet) {
-		return
-	}
-
-	let a = outlet.find("a").first()
-	a.html(`<i class="fas fa-spinner fa-fw fa-spin"></i>`)
-}
-
-function setButtonText(selector, text) {
-	let button = $(selector)
-	if (!button) {
-		return
-	}
-
-	let a = button.find("a").first()
-	a.text(text)
-
-	return button
-}
-
-async function sendMessage(num) {
-	if (isLoading) {
-		return
-	}
-	
-	isLoading = true
-
-	showSpinner(`[data-message=${num}]`)
-
-	try {
-		let resp = await $.ajax({
-			url: `${BASE_API_URL}/display`,
-			type: 'POST',
-			data: JSON.stringify({
+	},
+	display: {
+		send: async (num) => {
+			return await $.post(`${api.baseURL}/display`, JSON.stringify({
 				id: num
-			})
-		});
-
-		setButtonText(`[data-message=${num}]`, num)
-	
-		console.log(`Send message ${num}:`)
-		console.log(resp)
-	} catch (error) {
-		console.log(error)
+			}))
+		}
 	}
-
-	isLoading = false
 }
 
-async function loadState() {
-	let outlets = $('[data-outlet]')
-	for (const o of outlets) {
-		let outlet = $(o)
-		let num = outlet.data("outlet")
-		let power = await getPowerState(num)
+const dom = {
+	button: {
+		setText: (selector, text) => {
+			let button = $(selector)
+			if (!button) {
+				return
+			}
+	
+			let a = button.find("a").first()
+			a.text(text)
+	
+			return button
+		},
+		showSpinner: (selector) => {
+			let button = $(selector)
+			if (!button) {
+				return
+			}
+	
+			let a = button.find("a").first()
+			a.html(`<i class="fas fa-spinner fa-fw fa-spin"></i>`)
+	
+			return button
+		},
+
+	},
+	outlet: {
+		onLabel: "Turn On",
+		offLabel: "Turn Off",
+
+		on: function (num) {
+			let outlet = dom.button.setText(`[data-outlet=${num}]`, this.onLabel)
+			if (!outlet) {
+				return
+			}
 		
-		if (power) {
-			setButtonOff(num)
-		} else {
-			setButtonOn(num)
+			outlet.removeClass("tip warning").addClass("tip")
+		},
+		off: function (num) {
+			let outlet = dom.button.setText(`[data-outlet=${num}]`, this.offLabel)
+			if (!outlet) {
+				return
+			}
+		
+			outlet.removeClass("tip warning").addClass("warning")
+		},
+		getAll: () => {
+			return $('[data-outlet]')
 		}
+	},
+	message: {
+		getAll: () => {
+			return $('[data-message]')
+		}
+	},
+	show: () => {
+		$(".action-container").show()
+	}
+}
+
+const app = {
+	isLoading: false,
+
+	toggleOutlet: async (num) => {
+		if (app.isLoading) {
+			return
+		}
+	
+		app.isLoading = true
+		dom.button.showSpinner(`[data-outlet=${num}]`)
+	
+		try {
+			let currentState = await api.powerstrip.get(num)
+			let resp = await api.powerstrip.set(num, !currentState)
+	
+			if (!currentState) {
+				dom.outlet.off(num)
+			} else {
+				dom.outlet.on(num)
+			}
+		
+			console.log(`Toggle power state of outlet ${num}:`)
+			console.log(resp)
+		} catch (error) {
+			console.log(error)
+		}
+	
+		app.isLoading = false
+	},
+	sendMessage: async (num) => {
+		if (app.isLoading) {
+			return
+		}
+		
+		app.isLoading = true
+	
+		dom.button.showSpinner(`[data-message=${num}]`)
+	
+		try {
+			let resp = await api.display.send(num)
+	
+			dom.button.setText(`[data-message=${num}]`, num)
+		
+			console.log(`Send message ${num}:`)
+			console.log(resp)
+		} catch (error) {
+			console.log(error)
+		}
+	
+		app.isLoading = false
+	},
+	load: async () => {
+		if (app.isLoading) {
+			return
+		}
+
+		app.isLoading = true
+		let outlets = dom.outlet.getAll()
+		try {
+			for (const o of outlets) {
+				let outlet = $(o)
+				let num = outlet.data("outlet")
+				let power = await api.powerstrip.get(num)
+				
+				if (power) {
+					dom.outlet.off(num)
+				} else {
+					dom.outlet.on(num)
+				}
+			}
+		} catch (error) {
+			console.log(error)
+		}
+
+		app.isLoading = false
 	}
 }
 
 $(async function() {
 	try {
-		await $.getJSON(`${BASE_API_URL}/ping`)
+		await api.ping()
 	} catch (error) {
 		console.log("API not listening")
 		return
 	}
 
-	$(".action-container").show()
+	dom.show()
 
-	let outlets = $('[data-outlet]')
+	let outlets = dom.outlet.getAll()
 	for (const o of outlets) {
 		let outlet = $(o)
 		let num = outlet.data("outlet")
-		let power = await getPowerState(num)
+		let power = await api.powerstrip.get(num)
+			
+		if (power) {
+			dom.outlet.off(num)
+		} else {
+			dom.outlet.on(num)
+		}
 
 		outlet.find("a").first().on("click", () => {
-			togglePowerState(num)
+			app.toggleOutlet(num)
 		})
-		
-		if (power) {
-			setButtonOff(num)
-		} else {
-			setButtonOn(num)
-		}
 	}
 
-	let messages = $('[data-message]')
+	let messages = dom.message.getAll()
 	for (const m of messages) {
 		let message = $(m)
 		let num = message.data("message")
 		message.find("a").first().on("click", () => {
-			sendMessage(num)
+			app.sendMessage(num)
 		})
 	}
 	
-	setInterval(loadState, 30000)
+	setInterval(app.load, 30000)
 });
